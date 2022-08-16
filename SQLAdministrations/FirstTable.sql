3b1d9535c3c56286f690f125288ccc7a1a388726
Mulalo Ndou <mulalo.ndou@sambeconsulting.com>
2022-08-16 21:01:13 +0200

    myfirst table

---
+++ SQLAdministrations/FirstTable.sql
@@ -0,0 +1,19 @@
+USE [FirstDatabase]
+GO
+
+INSERT INTO [dbo].[Personal Info]
+           ([firstName]
+           ,[LastName]
+           ,[DoB]
+           ,[ID])
+     VALUES
+           ('Mulalo', 
+		   'ndou'
+           ,'01/01/2025'
+           ,777) 
+           
+GO
+ 
+
+
+
